9d86905756a35583d6f7c689eb5f27713d3d166f
KristiyanTodorof <kristiyantod10@gmail.com>
2026-01-07 00:59:55 +0200

    Adding BaseEntity models

---
+++ TopDriveX/TopDriveX.Domain/BaseEntity/BaseEntityGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDriveX.Domain.BaseEntity
+{
+    public abstract class BaseEntityGeneric<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
